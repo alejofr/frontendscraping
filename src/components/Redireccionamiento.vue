65e257b8784d13519861a026f3f306d7e9c6ce10
alejofr <61560586+alejofr@users.noreply.github.com>
2022-03-30 07:03:12 -0400

    actauliazado

---
+++ src/components/Redireccionamiento.vue
@@ -0,0 +1,16 @@
+<template>
+    <b-container class="bv-example-row py-5">
+        
+    </b-container>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Redireccionamiento',
+  created(){
+    //const code = this.$route.params;
+    console.log("hola como estas?");
+  }
+}
